1b24a1d3e27fec3f7744ebf8e210aea3ccd52d5e
HosseinYasinzadehh <hosseinyasinzadeh808@gmail.com>
2026-07-19 16:35:26 +0330

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+menu = """
+1. Add Contact
+2. Show Contacts
+3. Search Contact
+4. Delete Contact
+5. Exit
+"""
+
+while True:
+    print(menu)
+    user_choice = input("please enter yor choice: ")
