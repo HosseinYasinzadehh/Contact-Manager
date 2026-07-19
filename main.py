f52fb6f2438cd990cacc548c6a94f2f76bd206ab
HosseinYasinzadehh <hosseinyasinzadeh808@gmail.com>
2026-07-19 19:11:44 +0330

    feat: add contact complete

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 menu = """
 1. Add Contact
 2. Show Contacts
@@ -6,9 +8,36 @@ menu = """
 5. Exit
 """
 
+def add_contact():
+    contact_name = input("please enter contact name: ")
+    contact_phone = input("please enter contact phone number: ")
+    contact_mail = input("please enter contact mail: ")
+
+    new_contact = {
+             "name" : contact_name,
+            "phone" : contact_phone,
+            "mail" : contact_mail
+        }
+    
+
+    if len(contact_name) >= 3 and len(contact_phone)>= 8 and len(contact_mail) >= 8:
+        try:
+            with open("contacts.json","r") as contacts_file:
+                contacts = json.load(contacts_file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            contacts = []
+        contacts.append(new_contact)
+        with open("contacts.json","w") as contacts_file:
+            json.dump(contacts,contacts_file)
+        print("contact add!!!")
+    else:
+        print("enter valid input")
+
 while True:
     print(menu)
     user_choice = int(input("please enter yor choice: "))
 
     if user_choice == 5:
         break
+    elif user_choice == 1:
+        add_contact()
